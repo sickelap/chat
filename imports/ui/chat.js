bb81a1a48d86ee8f8ea2d07e9fa8c9ff578057c3
Genadijus Paleckis <genadijus.paleckis@gamesys.co.uk>
2016-04-26 07:15:57 +0100

    update app header

--- imports/ui/chat.js
+++ imports/ui/chat.js
@@ -2,6 +2,7 @@ import {Meteor} from 'meteor/meteor';
 import {Template} from 'meteor/templating';
 import {ReactiveDict} from 'meteor/reactive-dict';
 import './chat.html';
+import './header.html';
 
 import {Messages} from '../api/messages';
 
